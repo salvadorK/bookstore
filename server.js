95c940c07a110b5e201baaa443aa81c179220baa
Karl <karlchikc@gmail.com>
2019-08-12 13:37:30 -0400

    first commit -karl

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
-let express = require('express')
-let app = express()
-let reloadMagic = require('./reload-magic.js')
+let express = require("express");
+let app = express();
+let MongoClient = require("mongodb").MongoClient;
+let ObjectID = require("mongodb").ObjectID;
+let reloadMagic = require("./reload-magic.js");
+let multer = require("multer");
+let upload = multer({ dest: __dirname + "/uploads/" });
+reloadMagic(app);
 
-reloadMagic(app)
+app.use("/", express.static("build")); // Needed for the HTML and JS files
+app.unsubscribe("/uploads", express.static("uploads"));
+app.use("/", express.static("public")); // Needed for local assets
 
-app.use('/', express.static('build')); // Needed for the HTML and JS files
-app.use('/', express.static('public')); // Needed for local assets
-
+let dbo = undefined;
+let url =
+  "mongodb+srv://karlchikc:bobsuekc01@alibay-7bioh.mongodb.net/test?retryWrites=true&w=majority";
+MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+  dbo = db.db("alibay");
+});
+app.post("/sign-up", upload.none(), (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  dbo.collection("users").findOne({ username }, (err, user) => {
+    if (user) {
+      res.send(JSON.stringify({ success: false }));
+      return;
+    }
+    dbo.collection("users").insertOne({ username, password });
+    res.send(JSON.stringify({ success: true }));
+  });
+});
 // Your endpoints go after this line
 
 // Your endpoints go before this line
 
-app.all('/*', (req, res, next) => { // needed for react router
-    res.sendFile(__dirname + '/build/index.html');
-})
+app.all("/*", (req, res, next) => {
+  // needed for react router
+  res.sendFile(__dirname + "/build/index.html");
+});
 
-
-app.listen(4000, '0.0.0.0', () => { console.log("Server running on port 4000") })
+app.listen(4000, "0.0.0.0", () => {
+  console.log("Server running on port 4000");
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class App extends Component {
-    render = () => {
-        return "Hello world!"
+  constructor() {
+    super();
+    this.state = {
+      usernameInput: "",
+      passwordInput: "",
+      username: undefined
+    };
+  }
+  usernameChange = evt => {
+    this.setState({ usernameInput: evt.target.value });
+  };
+  passwordChange = evt => {
+    this.setState({ passwordInput: evt.target.value });
+  };
+  signUpsubmitHandler = async evt => {
+    evt.preventDefault();
+    console.log("username", this.state.username);
+    console.log("password", this.state.passwordInput);
+    let name = this.state.usernameInput;
+    let data = new FormData();
+    data.append("username", name);
+    data.append("password", this.state.passwordInput);
+    let response = await fetch("/sign-up", { method: "POST", body: data });
+    let body = await response.text();
+    console.log("/sign up response", body);
+    body = JSON.parse(body);
+    if (body.success) {
+      console.log("sign -up done");
+      return;
     }
+    alert("use a different username");
+  };
+
+  render = () => {
+    return (
+      <div>
+        <form onSubmit={this.signUpsubmitHandler}>
+          Username <input type="text" onChange={this.usernameChange} />
+          Password <input type="text" onChange={this.passwordChange} />
+          <input type="submit" value="sign-up" />
+        </form>
+      </div>
+    );
+  };
 }
 
-
-export default App
+export default App;
