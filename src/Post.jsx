062155cbb2338b4d0abb43d00524ea051208e690
Chi <karlchikc@gmail.com>
2019-08-14 16:58:30 -0400

    fixed err in post

--- src/Post.jsx
+++ src/Post.jsx
@@ -10,12 +10,11 @@ class Post extends Component {
 
   render = () => {
     return (
-      <div>   
+      <div>
         <h1>booktitle is:{this.props.contents.booktitle}</h1>
         <img src={this.props.contents.img} height="100px" />
         <div>isbn:{this.props.contents.isbn}</div>
-        <div>Price:{this.props.comtents.price}</div>
-        
+        <div>Price:{this.props.contents.price}</div>
       </div>
     );
   };
